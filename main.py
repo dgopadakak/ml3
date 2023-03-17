8cec0ef796a940882030e5d3070d57f8b69bafcb
dgopadakak <dgopadakakv3@gmail.com>
2023-03-17 20:12:21 +0300

    All works, now I'm rewriting comments

--- main.py
+++ main.py
@@ -19,6 +19,30 @@ def nan_to_zero(x):
     return x
 
 
+def no_yes_to_false_true(x):
+    if isinstance(x, str):
+        if x == 'no':
+            return False
+        elif x == 'yes':
+            return True
+    return x
+
+
+def eco_to_num(x):
+    if isinstance(x, str):
+        if x == 'no data':
+            return 0
+        if x == 'poor':
+            return 1
+        if x == 'satisfactory':
+            return 2
+        if x == 'good':
+            return 3
+        if x == 'excellent':
+            return 4
+    return x
+
+
 # 1 ################################
 # Считываем набор данных
 df = pd.read_csv('train_2.csv', sep=',', decimal='.', header=0)
@@ -69,37 +93,27 @@ plt.show()
 
 # Удаляем столбец даты
 df.drop(['timestamp'], axis=1, inplace=True)
+df.drop(['product_type'], axis=1, inplace=True)
+df.drop(['sub_area'], axis=1, inplace=True)
 # df = df.drop('C6H6(GT)', axis=1)                                   закомментил, так как не понял зачем это
 
 # Заменяем отсутствующие значения 0
 df = df.applymap(lambda x: nan_to_zero(x))
+df = df.applymap(lambda x: no_yes_to_false_true(x))
+df = df.applymap(lambda x: eco_to_num(x))
 
-# Заменяем значения 0 на среднее значение/медиану
+# Заменяем значения 0 на среднее значение
 
-for i in df[df['max_floor'] == 0].index:
-    df.loc[i, 'max_floor'] = df['max_floor'].mean()
-for i in df[df['material'] == 0].index:
-    df.loc[i, 'material'] = df['material'].mean()
-for i in df[df['build_year'] == 0].index:
-    df.loc[i, 'build_year'] = df['build_year'].mean()
-for i in df[df['PT08.S2(NMHC)'] == 0].index:
-    df.loc[i, 'PT08.S2(NMHC)'] = df['PT08.S2(NMHC)'].mean()
-for i in df[df['NOx(GT)'] == 0].index:
-    df.loc[i, 'NOx(GT)'] = df['NOx(GT)'].median()
-for i in df[df['PT08.S3(NOx)'] == 0].index:
-    df.loc[i, 'PT08.S3(NOx)'] = df['PT08.S3(NOx)'].mean()
-for i in df[df['NO2(GT)'] == 0].index:
-    df.loc[i, 'NO2(GT)'] = df['NO2(GT)'].mean()
-for i in df[df['PT08.S4(NO2)'] == 0].index:
-    df.loc[i, 'PT08.S4(NO2)'] = df['PT08.S4(NO2)'].median()
-for i in df[df['PT08.S5(O3)'] == 0].index:
-    df.loc[i, 'PT08.S5(O3)'] = df['PT08.S5(O3)'].median()
-for i in df[df['T'] == 0].index:
-    df.loc[i, 'T'] = df['T'].mean()
-for i in df[df['RH'] == 0].index:
-    df.loc[i, 'RH'] = df['RH'].mean()
-for i in df[df['AH'] == 0].index:
-    df.loc[i, 'AH'] = df['AH'].mean()
+tags = ['max_floor', 'material', 'build_year', 'num_room', 'kitch_sq', 'state', 'hospital_beds_raion', 'school_quota',
+        'cafe_sum_500_min_price_avg', 'cafe_sum_500_max_price_avg', 'cafe_avg_price_500', 'cafe_sum_1000_min_price_avg',
+        'cafe_sum_1000_max_price_avg', 'cafe_avg_price_1000', 'cafe_sum_1500_min_price_avg',
+        'cafe_sum_1500_max_price_avg', 'cafe_avg_price_1500', 'cafe_sum_2000_min_price_avg',
+        'cafe_sum_2000_max_price_avg', 'cafe_avg_price_2000', 'cafe_sum_3000_min_price_avg',
+        'cafe_sum_3000_max_price_avg', 'cafe_avg_price_3000', 'cafe_sum_5000_min_price_avg',
+        'cafe_sum_5000_max_price_avg', 'cafe_avg_price_5000']
+for tag in tags:
+    for i in df[df[tag] == 0].index:
+        df.loc[i, tag] = df[tag].mean()
 
 # 5 ################################
 
